e09ce501fc8ffe7429b60131567684613f3c3978
Terje Farup <TFarup@unibz.it>
2023-03-24 14:57:40 +0800

    consul agent API problems

--- tickets/src/index.js
+++ tickets/src/index.js
@@ -1,11 +1,47 @@
 const server = require('./server')
 
 const PORT = process.env.PORT || 8082
+const SERICE_NAME = process.env.SERICE_NAME || 'events'
 
+const Consul = require('consul')
+const consul = new Consul({ host: 'localhost', port: 8500 });
+
+
+const serviceName = SERICE_NAME
+const servicePort = PORT
+const healthCheckUrl = `http://localhost:${PORT}/health`;
+const healthCheckInterval = '10s';
+
+const check = {
+    http: healthCheckUrl,
+    interval: healthCheckInterval,
+};
+
+const details = {
+    name: serviceName,
+    port: servicePort,
+    check: check,
+};
+
+// register consul
+function registerService() {
+    try {
+        consul.agent.service.register(details);
+        console.log('Successfully registered with Consul');
+    } catch (error) {
+        console.error('Error registering with Consul:', error);
+    }
+}
+
+registerService()
 
 const startServer = () => {
-    server.listen(PORT, () =>
-        console.log(`Event server running at http://localhost:${PORT}`))
+    try {
+        server.listen(PORT, () =>
+            console.log(`Event server running at http://localhost:${PORT}`))
+    } catch (error) {
+        console.error('Error registering with Consul:', error);
+    }
 }
 
 startServer()
